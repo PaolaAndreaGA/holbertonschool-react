a9e833aae6e7193d15557e36f7e309c699475090
PaolaAndreaGA <paga0209@gmial.com>
2022-05-09 21:25:00 -0500

    update webpack.config

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,10 +1,31 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: '/js/dashboard_main.js',
+    mode: "production",
+    entry: "/js/dashboard_main.js",
     output: {
-	filename: 'bundle.js',
-	path: path.resolve(__dirname, 'public')
+	filename: "bundle.js",
+	path: path.resolve(__dirname, "public")
+    },
+    module: {
+	rules: [
+	    {
+		test: /\.css$/i,
+		use: ["style-loader", "css-loader"]
+	    },
+	    {
+		test: /\.(jpg|gif|png|jpeg|svg)$/i,
+		use: [
+		    "file-loader",
+		    {
+			loader: "image-webpack-loader",
+			options: {
+			    bypassOnDebug: true,
+			    disable: true
+			},
+		    },
+		],
+	    }
+	]
     }
-};
+}
